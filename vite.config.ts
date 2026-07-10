b1264f2156187fa4006df4660bed585e3f38008d
Luxine <dma04018@gmail.com>
2026-07-10 11:42:41 +0200

    Mejoras 12:00

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ export default defineConfig({
       input: {
         main: 'index.html',
         ddu2025: 'ddu2025.html',
-        ddu2026: 'ddu2026.html',
         next_chapter: 'next-chapter.html',
         secret_vault: 'secret-vault.html',
+        ddu2027: '2027.html',
       },
     },
   },
